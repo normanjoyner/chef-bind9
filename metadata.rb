09c18c28cffcd4525f49acbc583562e5c507f056
Norman Joyner <norman.joyner@gmail.com>
2014-09-10 11:35:03 -0400

    Bump minor version

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'norman.joyner@gmail.com'
 license          'GPLv2'
 description      'Installs/Configures bind9'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.2.0'
